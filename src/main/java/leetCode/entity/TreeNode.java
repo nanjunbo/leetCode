5a142581c1146f76c3f8adb3d5d574abb8ca6a78
nanjunbo <345855983@qq.com>
2020-04-13 22:22:02 +0800

    add

--- src/main/java/leetCode/entity/TreeNode.java
+++ src/main/java/leetCode/entity/TreeNode.java
@@ -2,7 +2,7 @@ package leetCode.entity;
 
 public class TreeNode {
     int val;
-    TreeNode left;
-    TreeNode right;
+    public TreeNode left;
+    public TreeNode right;
     TreeNode(int x) { val = x; }
 }
