6fb27c999b1ce42b97bbee6cb484d0010e6080b3
nanjunbo <345855983@qq.com>
2020-04-20 21:42:42 +0800

    add

---
+++ src/main/java/leetCode/program/Solution268.java
@@ -0,0 +1,35 @@
+package leetCode.program;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 给定一个包含 0, 1, 2, ..., n 中 n 个数的序列，找出 0 .. n 中没有出现在序列中的那个数。
+ *
+ * 示例 1:
+ *
+ * 输入: [3,0,1]
+ * 输出: 2
+ *
+ * 示例 2:
+ *
+ * 输入: [9,6,4,2,3,5,7,0,1]
+ * 输出: 8
+ *
+ * 说明:
+ * 你的算法应具有线性时间复杂度。你能否仅使用额外常数空间来实现?
+ */
+public class Solution268 {
+    public int missingNumber(int[] nums) {
+        Set<Integer> numSet = new HashSet<Integer>();
+        for (int num : nums) numSet.add(num);
+
+        int expectedNumCount = nums.length + 1;
+        for (int number = 0; number < expectedNumCount; number++) {
+            if (!numSet.contains(number)) {
+                return number;
+            }
+        }
+        return -1;
+    }
+}
