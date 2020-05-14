2239c8428518034c4e689aed31cb80c11401d49d
nanjunbo <345855983@qq.com>
2020-05-14 22:36:03 +0800

    add

--- src/main/java/leetCode/program/Solution35.java
+++ src/main/java/leetCode/program/Solution35.java
@@ -3,50 +3,46 @@ package leetCode.program;
 import leetCode.entity.ListNode;
 
 /**
- * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+ * 给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组。
  *
- * 你可以假设数组中无重复元素。
+ * 说明:
  *
- * 示例 1:
+ *     初始化 nums1 和 nums2 的元素数量分别为 m 和 n 。
+ *     你可以假设 nums1 有足够的空间（空间大小大于或等于 m + n）来保存 nums2 中的元素。
+ * 示例:
  *
- * 输入: [1,3,5,6], 5
- * 输出: 2
+ * 输入:
+ * nums1 = [1,2,3,0,0,0], m = 3
+ * nums2 = [2,5,6],       n = 3
  *
- * 示例 2:
+ * 输出: [1,2,2,3,5,6]
  *
- * 输入: [1,3,5,6], 2
- * 输出: 1
- *
- * 示例 3:
- *
- * 输入: [1,3,5,6], 7
- * 输出: 4
- *
- * 示例 4:
- *
- * 输入: [1,3,5,6], 0
- * 输出: 0
  *
  */
 public class Solution35 {
-    public int searchInsert(int[] nums, int target) {
-        int first = 0;
-        int last = nums.length;
-        while (first < last) {
-            int index = (first + last) / 2;
-            if (nums[index] > target) {
-                last = index;
-            } else if (nums[index] < target) {
-                first = index +1;
-            } else {
-                return index;
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int[] nums = new int[m];
+        System.arraycopy(nums1,0,nums,0,m);
+        int p1=0,p2=0,p=0;
+        while(p1<m&&p2<n){
+            if(nums[p1]<=nums2[p2]){
+                nums1[p]=nums[p1];
+                p1++;
+            }else{
+                nums1[p]=nums2[p2];
+                p2++;
             }
+            p++;
         }
-        return first;
-    }
-
-    public static void main(String[] args) {
-        Solution35 x = new Solution35();
-        System.out.println(x.searchInsert(new int[]{1,3,5,6}, 7));
+        while(p1<m){
+            nums1[p]=nums[p1];
+            p1++;
+            p++;
+        }
+        while(p2<n){
+            nums1[p]=nums2[p2];
+            p2++;
+            p++;
+        }
     }
 }
